f5ac281fefeb650150b1b011eb1882697b288872
hibby <hibaqku7@gmail.com>
2025-06-20 11:38:16 +0300

    add heropower table with validation and relationship

--- server/models.py
+++ server/models.py
@@ -6,6 +6,9 @@ class Hero(db.Model):
     name =db.Column (db.String)
     super_name=db.Column(db.String)
 
+    hero_powers = db.relationship('HeroPower', backref='hero', cascade='all, delete')
+
+
 
 
 class Power (db.Model):
@@ -18,4 +21,20 @@ class Power (db.Model):
     def validate_description(self, key, value):
         if not value or len(value) < 20:
             raise ValueError("Description must be at least 20 characters long")
+        return value
+    
+
+
+class HeroPower(db.Model):
+    __tablename__="hero_powers"
+    id = db.Column(db.Integer, primary_key =True)
+    strength =db.Column(db.String)
+    hero_id =db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id =db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+
+    @validates("strength")
+    def validate_strength(self, key, value):
+        if value not in ['Strong', 'Weak', 'Average']:
+            raise ValueError("Strength must be 'Strong', 'Weak', or 'Average'")
         return value
